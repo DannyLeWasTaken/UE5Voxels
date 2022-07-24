5ab6ca925465d65dfb2f2c0f3c831d913ed5d456
DannyLeWasTaken <DannyLeWasTaken@protonmail.com>
2022-07-23 22:00:45 -0400

    Add ProceduralMeshComponent dependency

--- Source/UE5Voxels/UE5Voxels.Build.cs
+++ Source/UE5Voxels/UE5Voxels.Build.cs
@@ -9,5 +9,7 @@ public class UE5Voxels : ModuleRules
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay" });
+		
+		PrivateDependencyModuleNames.AddRange(new string[] { "ProceduralMeshComponent" });
 	}
 }
